6261d56fd9cea0ff426d9737fc965f9687ffa754
JulietaBeltrame <beltramejulieta@gmail.com>
2024-05-12 17:12:53 -0300

    agregar pag reservas

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const categorias = document.querySelector('.categorias');
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
 menuToggle.addEventListener('click', () => {
     categorias.classList.toggle('active');
     if (categorias.classList.contains('active')) {
@@ -9,3 +13,22 @@ menuToggle.addEventListener('click', () => {
         categorias.style.display = 'none';
     }
 });
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
